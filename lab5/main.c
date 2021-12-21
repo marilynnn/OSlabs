c3784b9929612a5e557618c2a2bc959881bebfe0
marilynnn <dzerassa73@gmail.com>
2021-12-21 21:58:08 +0300

    works

---
+++ lab5/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <pthread.h>
+
+pthread_mutex_t mutex;
+int count = 0;
+
+void* writer() {
+
+	while(1){
+		pthread_mutex_lock(&mutex);
+		++count;
+		pthread_mutex_unlock(&mutex);
+		sleep(1);
+	}
+}
+
+void* reader() {
+		
+        while(1){
+                pthread_mutex_lock(&mutex);
+                printf("TID: %u. Count is: %d.\n",pthread_self(),count);
+                pthread_mutex_unlock(&mutex);
+		sleep (1);
+	}
+}
+
+int main (){
+	pthread_t w, r[10];
+	pthread_mutex_init(&mutex, NULL);
+
+	pthread_create(&w, NULL, writer, NULL);
+	for (int i = 0; i < 10; ++i){
+		pthread_create(&r[i], NULL, reader, NULL);
+	}
+
+	pthread_join(w,NULL);
+	for (int i = 0; i < 10; ++i){ 
+		pthread_join(r[i],NULL);
+	}
+
+	return 0;
+}
